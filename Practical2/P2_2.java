ec2a3939e1af7f12c8347d1f6adfe4400fb982dc
Dhruv Dhaduk <dhadukd44@gmail.com>
2023-06-16 18:39:01 +0530

    Completed P2_2

---
+++ Practical2/P2_2.java
@@ -0,0 +1,35 @@
+import java.util.Scanner;
+
+public class P2_2
+{
+    public static void main(String[] args)
+    {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter String 1 : ");
+        String s1 = sc.nextLine();
+
+        System.out.print("Enter String 2 : ");
+        String s2 = sc.nextLine();
+
+        String prefix = s1.substring(0,  commonPrefixIndex(s1, s2));
+
+        System.out.println();
+        System.out.println("Largest Common prefix of two Strings is : " + prefix);
+    }
+
+    private static int commonPrefixIndex(String s1, String s2)
+    {
+        int l = Math.min(s1.length(), s2.length());
+
+        for (int i = 0; i < l; i++)
+        {
+            if (s1.charAt(i) != s2.charAt(i))
+            {
+                return i;
+            }
+        }
+
+        return l;
+    }
+}

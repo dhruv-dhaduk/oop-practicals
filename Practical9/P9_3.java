f2f251c2d7b4825b862fba1b2490a86af7d76282
Dhruv Dhaduk <dhadukd44@gmail.com>
2023-06-17 23:33:51 +0530

    Completed P9_3

--- Practical9/P9_3.java
+++ Practical9/P9_3.java
@@ -1,7 +1,48 @@
+import java.util.Scanner;
+
 public class P9_3
 {
     public static void main(String[] args)
     {
-        System.out.println("Comming soon . . . . . . . . .");
+        Scanner sc = new Scanner(System.in);
+
+        String[] list = new String[10];
+        for (int i = 0; i < 10; i++)
+        {
+            System.out.print("Enter list item #" + i + " : ");
+            list[i] = sc.nextLine();
+        }
+        System.out.println();
+
+        System.out.print("Enter string to search : ");
+        String key = sc.nextLine();
+        sc.close();
+        int index = binarySearch(list, key);
+        System.out.println("Index : " + index);
+    }
+
+    public static <E extends Comparable<E>> int binarySearch(E[] list, E key) {
+        int low = 0;
+        int high = list.length - 1;
+        
+        while (low <= high) {
+            int mid = (low + high) / 2;
+            int comparison = key.compareTo(list[mid]);
+            
+            if (comparison == 0) 
+            {
+                return mid;
+            } 
+            else if (comparison < 0) 
+            {
+                high = mid - 1;
+            } 
+            else 
+            {
+                low = mid + 1;
+            }
+        }
+        
+        return -1;
     }
 }

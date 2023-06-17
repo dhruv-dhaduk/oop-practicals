87dfd99e693cdc23ce507fdffc5571e04e76835c
Dhruv Dhaduk <dhadukd44@gmail.com>
2023-06-17 16:58:21 +0530

    Completed P6_2

--- Practical6/P6_2.java
+++ Practical6/P6_2.java
@@ -1,7 +1,38 @@
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
 public class P6_2
 {
     public static void main(String[] args)
     {
+        Scanner sc = new Scanner(System.in);
+
+        int a, b;
+
+        while (true)
+        {
+            try
+            {
+                System.out.print("Enter a : ");
+                a = sc.nextInt();
+                sc.nextLine();
+
+                System.out.print("Enter b : ");
+                b = sc.nextInt();
+                sc.nextLine();
+            }
+            catch(InputMismatchException e)
+            {
+                sc.nextLine();
+                System.out.println("Input Mismatch");
+                System.out.println();
+                continue;
+            }
+            break;
+        }
         
+        sc.close();
+
+        System.out.println(a + " + " + b + " = " + (a + b));
     }
 }

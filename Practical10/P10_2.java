32b1fe5b02d264c28c874090288e4f81fb6f69a4
Dhruv Dhaduk <dhadukd44@gmail.com>
2023-06-17 23:51:26 +0530

    Completed P10_2

--- Practical10/P10_2.java
+++ Practical10/P10_2.java
@@ -1,7 +1,38 @@
+import java.util.PriorityQueue;
+
 public class P10_2
 {
     public static void main(String[] args)
     {
-        System.out.println("Comming soon . . . . . . . . .");
+        PriorityQueue<String> queue1 = new PriorityQueue<>();
+        queue1.add("George");
+        queue1.add("Jim");
+        queue1.add("John");
+        queue1.add("Blake");
+        queue1.add("Kevin");
+        queue1.add("Michael");
+
+        PriorityQueue<String> queue2 = new PriorityQueue<>();
+        queue2.add("George");
+        queue2.add("Katie");
+        queue2.add("Kevin");
+        queue2.add("Michelle");
+        queue2.add("Ryan");
+
+        PriorityQueue<String> union = new PriorityQueue<>(queue1);
+        union.addAll(queue2);
+
+        PriorityQueue<String> difference = new PriorityQueue<>(queue1);
+        difference.removeAll(queue2);
+
+        PriorityQueue<String> intersection = new PriorityQueue<>(queue1);
+        intersection.retainAll(queue2);
+
+        union.removeAll(intersection);
+        union.addAll(intersection);
+
+        System.out.println("Union: " + union);
+        System.out.println("Difference: " + difference);
+        System.out.println("Intersection: " + intersection);
     }
 }

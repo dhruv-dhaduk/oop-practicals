7d81cc28a6ec7001b94933938b239227e2734533
Dhruv Dhaduk <dhadukd44@gmail.com>
2023-06-17 23:17:23 +0530

    Completed P9_1

--- Practical9/P9_1.java
+++ Practical9/P9_1.java
@@ -1,7 +1,31 @@
+import java.util.Scanner;
+
 public class P9_1
 {
     public static void main(String[] args)
     {
-        System.out.println("Comming soon . . . . . . . . .");
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter a number : ");
+        int n = sc.nextInt();
+        sc.close();
+
+        System.out.println("Binary : " + convertToBinary(n));
+    }
+
+    public static String convertToBinary(int number) 
+    {
+
+        if (number == 0) 
+        {
+            return "0";
+        } 
+        else if (number == 1) 
+        {
+            return "1";
+        }
+        else {
+            return convertToBinary(number / 2) + number % 2;
+        }
     }
 }
